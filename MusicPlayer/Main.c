c595c254502a0862d45829b87932f771cadd9f52
Mark Gadsby <Mark.R.Gadsby@GMail.Com>
2020-04-01 08:23:05 +0100

    Added music player

--- MusicPlayer/Main.c
+++ MusicPlayer/Main.c
@@ -17,46 +17,74 @@
 ///////////////////////////////////////////////////////////////////////
 #include <alsa/asoundlib.h>
 #include <vorbis/vorbisfile.h>
+#include <linux/limits.h>
+#include <dirent.h>
 #include "MusicPlayer.h"
 
 const int SAMPLE_RATE = 44100;          // frequency in Hz
 
 int main()
 {
-    snd_pcm_t* pcmHandle = NULL;        // PCM handle
-
-    OpenPCM(&pcmHandle);
-    unsigned long pcmPeriodSize = SetupPCM(pcmHandle);
+    DIR*            dir;
+    struct dirent*  ent;
+    char trackArray[30][NAME_MAX];
+    memset(trackArray, 0, 30 * NAME_MAX);
     
-    // Allocate buffer to hold a single PCM period
-    // bufferSize = PCM period * 2 channels * 2 byte format
-    int bufferSize = pcmPeriodSize * 2 * 2;
-    char* buffer = (char*) malloc(bufferSize);
-
-    OggVorbis_File vf;
-
-    if (ov_fopen("TrashShort.ogg", &vf) < 0)
-        printf("ov_fopen error\n");
-
-    int current_section = 0;
-    long bytesRead = 0;
-
-    do  // pass a buffer of data from the file to the PCM 
+    if (dir = opendir ("/home/mark/Music/Rival Consoles/Persona"))
     {
-        bytesRead = ov_read(&vf, buffer, bufferSize, 0, 2, 1, &current_section);
+        int rowIndex = 0;
+        while (ent = readdir(dir)) 
+        {
+            if (strstr(ent->d_name, ".ogg"))   
+                strcpy(&trackArray[rowIndex++][0],ent->d_name);
+        }
+        closedir(dir);
+    } 
 
-        // bytesRead will four times as large as snd_pcm_writei is expecting 
-        // (2 channels, 2 byte data format)  
-        if (snd_pcm_writei(pcmHandle, buffer, bytesRead / 4) < 0) 
-            break;
-    }
-    while(bytesRead);
+    chdir("/home/mark/Music/Rival Consoles/Persona");
 
-    ov_clear(&vf);              // clear the decoder's buffers
-    free(buffer);               // free the buffer memory
-    snd_pcm_drain(pcmHandle);   // stop a PCM preserving pending frames
-    snd_pcm_close(pcmHandle);   // close PCM handle 
+
+    int i = 0;
+
+    while (trackArray[i][0])
+    {
+        printf("%d\t-\t%s\n", i, &trackArray[i][0]);
     
+        snd_pcm_t* pcmHandle = NULL;        // PCM handle
+
+        OpenPCM(&pcmHandle);
+        unsigned long pcmPeriodSize = SetupPCM(pcmHandle);
+        
+        // Allocate buffer to hold a single PCM period
+        // bufferSize = PCM period * 2 channels * 2 byte format
+        int bufferSize = pcmPeriodSize * 2 * 2;
+        char* buffer = (char*) malloc(bufferSize);
+
+        OggVorbis_File vf;
+
+        if (ov_fopen(&trackArray[i++][0], &vf) < 0)
+            printf("ov_fopen error\n");
+
+        int current_section = 0;
+        long bytesRead = 0;
+
+        do  // pass a buffer of data from the file to the PCM 
+        {
+            bytesRead = ov_read(&vf, buffer, bufferSize, 0, 2, 1, &current_section);
+
+            // bytesRead will four times as large as snd_pcm_writei is expecting 
+            // (2 channels, 2 byte data format)  
+            if (snd_pcm_writei(pcmHandle, buffer, bytesRead / 4) < 0) 
+                break;
+        }
+        while(bytesRead);
+
+        ov_clear(&vf);              // clear the decoder's buffers
+        free(buffer);               // free the buffer memory
+        snd_pcm_drain(pcmHandle);   // stop a PCM preserving pending frames
+        snd_pcm_close(pcmHandle);   // close PCM handle 
+    }
+
     return 1;
 }
 
