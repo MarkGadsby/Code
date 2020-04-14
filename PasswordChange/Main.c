1a8184edf38cd0184c0ef0ed9a29326fda2f9f10
Mark Gadsby <Mark.R.Gadsby@GMail.Com>
2020-04-14 14:09:11 +0100

    Added a C version of password change

--- PasswordChange/Main.c
+++ PasswordChange/Main.c
@@ -2,24 +2,32 @@
 
 int main()
 {
-    FILE* fp;
-    fp = fopen("Passwords.txt", "r");
+    FILE* fp = fopen("Passwords.txt", "r");
 
-    if (fp != NULL)
+    if (fp == NULL)
     {
-        char userName[64];
-        char password[64];
-        
-        fgets(userName, 64, fp);
-        printf("%s", userName);
-        fgets(userName, 64, fp);
-        printf("%s", userName);
-        fgets(userName, 64, fp);
-        printf("%s", userName);
-   
-        printf("%s", userName);
-        fclose(fp);
+        printf("Couldn't find the Passwords file on your disk\n");
+        return 0;
     }
-   
+
+    char userName[64][64];
+    char password[64][64];
+
+    int iNumberofPasswords = 0;
+
+    while (!feof(fp))
+    {
+        fgets(&userName[iNumberofPasswords][0], 64, fp);
+        fgets(&password[iNumberofPasswords][0], 64, fp);
+        iNumberofPasswords++;
+    } 
+
+    for (int i; i <= iNumberofPasswords; i++)
+    {
+        printf("%s", &userName[i][0]);
+        printf("%s", &password[i][0]);
+    }
+
+    fclose(fp);
     return(0);
 }
