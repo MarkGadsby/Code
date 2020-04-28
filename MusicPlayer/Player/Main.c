32051b9d1aadf27d3a55a728f27b91f1738cd4ac
Mark Gadsby <Mark.R.Gadsby@GMail.Com>
2020-04-28 07:07:32 +0100

    Put MusicPlayer back to needing complete path

--- MusicPlayer/Player/Main.c
+++ MusicPlayer/Player/Main.c
@@ -9,7 +9,7 @@
 //
 // To build this file:
 //
-// gcc -W Main.c -o MusicPlayer -lasound -lvorbisfile -lmysqlclient
+// gcc -W Main.c -o MusicPlayer -lasound -lvorbisfile
 //
 // This file will use the alsa lib to set up the pulse-code modulation interface 
 // (PCM) to play output and use the vorbis lib to read in a .ogg file and pass
@@ -21,7 +21,6 @@
 #include <dirent.h>
 #include "MusicPlayer.h"
 #include <stdbool.h>
-#include <mysql/mysql.h>
 
 const int SAMPLE_RATE = 44100;          // frequency in Hz
 
@@ -29,13 +28,12 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("\nUseage: ./MusicPlayer \"Artist\"\n\n");
+        printf("\nUseage: ./MusicPlayer \"/home/[user]/Music/Artist/Album/\"\n\n");
         return 0;
     }
 
     char PlayPath[PATH_MAX];
-
-    GetPathFromArtist(PlayPath, argv[1]);
+    strcpy(PlayPath, argv[1]);
 
     struct TrackInfo trackArray[30]; // Allocate track info
     memset(trackArray, 0, 30 * sizeof(struct TrackInfo));
@@ -224,45 +222,4 @@ void BubbleSortTracks(struct TrackInfo trackArray[30], int total)
             } 
         }    
     }
-}
-
-void GetPathFromArtist(char path[PATH_MAX], char* artist)
-{
-    char QueryString[PATH_MAX] = {"SELECT Path FROM Albums WHERE Artist = \""};
-
-    strcat(QueryString, artist);
-    strcat(QueryString, "\"");
-
-    MYSQL* pConnection = mysql_init(NULL);
-
-    if (!mysql_real_connect(pConnection, "localhost" , "root" , "root", "Albums", 0 , NULL, 0))
-    {
-        printf("%s\n", mysql_error(pConnection));
-        mysql_close(pConnection);
-    }
-
-    if (mysql_query(pConnection, QueryString))
-    {
-        printf("%s\n", mysql_error(pConnection));
-        mysql_close(pConnection);
-    }
-
-    MYSQL_RES* result = mysql_store_result(pConnection);
-  
-    if (!result) 
-    {
-        printf("%s\n", mysql_error(pConnection));
-        mysql_close(pConnection);
-    }
-
-    MYSQL_ROW row;
-    
-    while ((row = mysql_fetch_row(result))) 
-    { 
-        strcpy(path, *row);
-        strcat(path, "/");
-    }
-
-    mysql_free_result(result);
-    mysql_close(pConnection);
 }
